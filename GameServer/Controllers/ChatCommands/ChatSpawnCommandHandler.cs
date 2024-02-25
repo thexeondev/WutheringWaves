70e2e21a19ba5488667be723073e0e3d69f0cecd
AliasPedroKarim <karimisma6464@gmail.com>
2024-02-25 20:10:45 +0100

    Refactor ChatSpawnCommandHandler to allow spawning monsters at player's location

--- GameServer/Controllers/ChatCommands/ChatSpawnCommandHandler.cs
+++ GameServer/Controllers/ChatCommands/ChatSpawnCommandHandler.cs
@@ -28,19 +28,33 @@ internal class ChatSpawnCommandHandler
     }
 
     [ChatCommand("monster")]
-    [ChatCommandDesc("/spawn monster [id] [x] [y] [z] - spawns monster with specified id and coordinates")]
-    public async Task OnSpawnMonsterCommand(string[] args)
+    [ChatCommandDesc("/spawn monster [id] ([x] [y] [z] | here) - spawns monster with specified id and coordinates")]
+    public void OnSpawnMonsterCommand(string[] args)
     {
+        if (args.Length == 2 && int.TryParse(args[0], out int levelEntityId1) && args[1] == "here")
+        {
+            PlayerEntity? entity = _creatureController.GetPlayerEntity();
+            if (entity == null) return;
+
+            _ = SpawnMonster(levelEntityId1, entity.Pos.X / 100, entity.Pos.Y / 100, entity.Pos.Z / 100);
+            return;
+        }
+
         if (args.Length != 4 ||
             !(int.TryParse(args[0], out int levelEntityId) &&
             float.TryParse(args[1], out float x) &&
             float.TryParse(args[2], out float y) &&
             float.TryParse(args[3], out float z)))
         {
-            _helperRoom.AddMessage(1338, 0, "Usage: /spawn monster [id] [x] [y] [z]");
+            _helperRoom.AddMessage(1338, 0, "Usage: /spawn monster [id] ([x] [y] [z] | here)");
             return;
         }
 
+        _ = SpawnMonster(levelEntityId, x, y, z);
+    }
+
+    private async Task SpawnMonster(int levelEntityId, float x, float y, float z)
+    {
         MonsterEntity monster = _entityFactory.CreateMonster(levelEntityId);
         monster.Pos = new()
         {
